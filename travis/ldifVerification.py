cc4e0d756800496f5287d963987c0b0c2f1f8913
Hctor Rodr¡guez <hector.rodriguez@deimos-space.com>
2020-04-27 18:14:24 +0000

    Added tests for LDIF Validation

---
+++ travis/ldifVerification.py
@@ -0,0 +1,41 @@
+from ldif3 import LDIFParser
+from os import sys
+
+def import_ldif():
+    ldif_mappings = {
+        "default": [
+            "base.ldif",
+            "attributes.ldif",
+            "scopes.ldif",
+            "scripts.ldif",
+            "configuration.ldif",
+            "scim.ldif",
+            "oxidp.ldif",
+            "oxtrust_api.ldif",
+            "passport.ldif",
+            "oxpassport-config.ldif",
+            "gluu_radius_base.ldif",
+            "gluu_radius_server.ldif",
+            "clients.ldif",
+            "oxtrust_api_clients.ldif",
+            "scim_clients.ldif",
+            "o_metric.ldif",
+            "gluu_radius_clients.ldif",
+            "passport_clients.ldif",
+            "scripts_casa.ldif",
+        ]
+    }
+    exitCode = 0
+    for file_ in ldif_mappings["default"]:
+        print("Checking {} file...".format(file_))
+        src = "../templates/ldif/{}".format(file_)
+        try:
+            parser = LDIFParser(open(src))
+            print("Successfully tested",file_)
+        except:
+            print(file_," validation failed")
+            exitCode = 1
+        print("--------------------")
+    return exitCode
+
+import_ldif()
